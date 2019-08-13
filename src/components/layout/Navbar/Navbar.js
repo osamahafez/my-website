c1d84ad5c43bae839784e6bfa811fbeecd12de19
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-08-13 02:38:48 +0200

    change navbar styling

--- src/components/layout/Navbar/Navbar.js
+++ src/components/layout/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Navbar.css';
 // import { NavHashLink as NavLink } from 'react-router-hash-link';
 import { NavLink } from 'react-router-dom'; // use the above after finishing the page
 
-const Navbar = () => {
+const Navbar = (props) => {
     return (
-        <nav className="navbar navbar-expand-md navbar-dark bg-primary">
-            <span className="navbar-brand" style={{cursor:'default'}}>Osama Hafez</span>
+        <nav className="navbar navbar-expand-md">
+            <span className="navbar-brand text-light" style={{cursor:'default'}}>Osama Hafez</span>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarElements" aria-controls="navbarElements" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon"><i className="fas fa-bars fa-lg fa-2x text-light"></i></span>
             </button>
 
                 <div className="collapse navbar-collapse" id="navbarElements">
@@ -15,19 +16,19 @@ const Navbar = () => {
                         <li className="nav-item">
                             {/* <NavLink smooth className="nav-link" to="/education#education">Education</NavLink> */}
                             {/* use the above after finishing the page */}
-                            <NavLink className="nav-link" to="/education">Education</NavLink>
+                            <NavLink className="nav-link text-light" to="/education">Education</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/experience">Experience</NavLink>
+                            <NavLink className="nav-link text-light" to="/experience">Experience</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/projects">Projects</NavLink>
+                            <NavLink className="nav-link text-light" to="/projects">Projects</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/skills">Skills</NavLink>
+                            <NavLink className="nav-link text-light" to="/skills">Skills</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/activites">Extracurricular Activities</NavLink>
+                            <NavLink className="nav-link text-light" to="/activites">Extracurricular Activities</NavLink>
                         </li>
                     </ul>
         
