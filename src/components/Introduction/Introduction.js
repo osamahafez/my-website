cf262cf51253d831374370aa32e2938c67f40b39
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-08-16 01:02:30 +0200

    remove profile pic from intro

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Introduction.css';
 import { HashLink as Link } from 'react-router-hash-link';
-import profileImg from '../../images/osama_pic.png';
+// import profileImg from '../../images/osama_pic.png';
 import Navbar from '../layout/Navbar/Navbar';
 import SocialLinks from '../SocialLinks/SocialLinks';
 
@@ -10,13 +10,13 @@ const Introduction = (props) => {
         <div id='introduction' className='intro'>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-12 text-center mt-3 mb-5">
+                    <div className="col-md-12 text-center mt-3 mb-5 intro-content">
                         <Navbar />
-                        <img src={profileImg} alt="profile_pic" className='profile-img rounded-circle mx-auto d-block' />
+                        {/* <img src={profileImg} alt="profile_pic" className='profile-img rounded-circle mx-auto d-block' /> */}
                         <h1 className="display-2 text-light">Hello, I'm Osama</h1>
                         <h1 className="display-4 text-light">I'm a full stack developer</h1>
                         <SocialLinks color='text-info' mb='mb-5' mt='mt-5' />
-                        <Link smooth className="btn btn-primary btn-lg" to="/projects#projects">My Work</Link>
+                        <Link smooth className="btn btn-primary btn-lg work-btn" to="/projects#projects">My Work <i className="fas fa-arrow-alt-circle-down"></i></Link>
                     </div>
 
                 </div>
