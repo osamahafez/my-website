7fb49c98ff9253d94fe5cd3c1db2b6331cbe3b48
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-08-18 00:59:48 +0200

    animate my work button

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Introduction.css';
 import { HashLink as Link } from 'react-router-hash-link';
-// import profileImg from '../../images/osama_pic.png';
 import Navbar from '../layout/Navbar/Navbar';
 import SocialLinks from '../SocialLinks/SocialLinks';
+import { Spring } from 'react-spring/renderprops';
+import VisibilitySensor from 'react-visibility-sensor';
+
 
 const Introduction = (props) => {
     return (
@@ -14,11 +16,32 @@ const Introduction = (props) => {
                     <div className="row">
                         <div className="col-md-12 text-center mt-3 mb-5 intro-content">
                             <Navbar />
-                            {/* <img src={profileImg} alt="profile_pic" className='profile-img rounded-circle mx-auto d-block' /> */}
                             <h1 className="display-2 text-light" style={{paddingTop:'30px'}}>Hello, I'm Osama.</h1>
                             <h1 className="display-4 text-light">A full stack developer</h1>
                             <SocialLinks color='text-light' mb='mb-5' mt='mt-5' />
-                            <Link smooth className="btn btn-danger btn-lg work-btn" to="/projects#projects">My Work <i className="fas fa-arrow-alt-circle-down"></i></Link>
+
+                                <VisibilitySensor partialVisibility>
+                                    {({isVisible}) => 
+                                        
+                                        <Spring  
+                                            to={{
+                                                opacity: isVisible ? 1 : 0, 
+                                                transform: isVisible ? "translateY(0px)" : "translateY(-200px)"
+                                            }} 
+                                            delay={300}
+                                        >
+                                            {props => (
+                                                <div style={props}>
+                                                    <Link smooth className="btn btn-danger btn-lg work-btn" to="/projects#projects">My Work <i className="fas fa-arrow-alt-circle-down"></i></Link>
+                                                </div>
+                                            )}
+                                        </Spring>
+
+                                    }
+                                </VisibilitySensor>
+
+                               
+                           
                         </div>
 
                     </div>
